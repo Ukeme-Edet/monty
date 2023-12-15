c60b24bdbd7406627331a9a187c56a4141637c76
Ukeme Edet <ukemeedet2207@gmail.com>
2023-12-15 21:42:59 +0100

    Tried :)

--- main.c
+++ main.c
@@ -1,61 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "monty.h"
 
 /**
- * main - The programs main entrypoint
- * @argc: The number of arguments passed to the program
- * @argv: The arguments passed to the program
- * Return: 0 on success, 1 on failure
+ * error_usage - prints usage message and exits
+ *
+ * Return: nothing
  */
-int main(int argc, char *argv[])
+void error_usage(void)
 {
-	/* stack_t *stack = initialise_stack(); */
-	FILE *file;
-	size_t i;
-	stack_t *monty_stack = initialise_stack();
-	char line[1024], **monty_tokens;
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
-	}
-	for (i = 0; fgets(line, 1024, file) != NULL && ++i; i++)
-	{
-		monty_tokens = parse_line(line);
-		if (monty_tokens[0] == NULL)
-			continue;
-		execute(monty_tokens, monty_stack, i);
-	}
-	return (0);
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
 }
 
 /**
- * execute - executes a line of monty code
- * @tokens: array of strings
- * @stack: pointer to stack
- * @line_number: line number
+ * file_error - prints file error message and exits
+ * @argv: argv given by manin
+ *
+ * Return: nothing
  */
-void execute(char **tokens, stack_t *stack, unsigned int line_number)
+void file_error(char *argv)
 {
-	int i;
-	instruction_t instructions[] = {
-		{"push", stack_push},
-		{"pall", stack_pall},
-		{NULL, NULL}};
-	for (i = 0; instructions[i].opcode != NULL; i++)
-	{
-		if (strcmp(tokens[0], instructions[i].opcode) == 0)
-		{
-			instructions[i].f(&stack, line_number);
-			return;
-		}
-	}
-	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, tokens[0]);
+	fprintf(stderr, "Error: Can't open file %s\n", argv);
 	exit(EXIT_FAILURE);
+}
+
+int status = 0;
+/**
+ * main - entry point
+ * @argv: list of arguments passed to our program
+ * @argc: ammount of args
+ *
+ * Return: nothing
+ */
+int main(int argc, char **argv)
+{
+	FILE *file;
+	size_t buf_len = 0;
+	char *buffer = NULL;
+	char *str = NULL;
+	stack_t *stack = NULL;
+	unsigned int line_cnt = 1;
+
+	global.data_struct = 1;
+	if (argc != 2)
+		error_usage();
+
+	file = fopen(argv[1], "r");
+
+	if (!file)
+		file_error(argv[1]);
+
+	while (getline(&buffer, &buf_len, file) != -1)
+	{
+		if (status)
+			break;
+		if (*buffer == '\n')
+		{
+			line_cnt++;
+			continue;
+		}
+		str = strtok(buffer, " \t\n");
+		if (!str || *str == '#')
+		{
+			line_cnt++;
+			continue;
+		}
+		global.argument = strtok(NULL, " \t\n");
+		opcode(&stack, str, line_cnt);
+		line_cnt++;
+	}
+	free(buffer);
+	free_stack(stack);
+	fclose(file);
+	exit(status);
 }
